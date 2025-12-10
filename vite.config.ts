08ca0c6e8bd936aad44fa9deb69d19cfb7f84aef
Ns. M. Arief Farhiansyah <fardianarief@gmail.com>
2025-12-10 17:26:30 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   // PENTING: Ganti 'NAMA-REPOSITORY' dengan nama repository GitHub Anda.
   // Contoh: Jika repo Anda 'portfolio-fardian', maka menjadi base: '/portfolio-fardian/'
   // Jika Anda deploy ke user.github.io (root), hapus baris base ini atau set ke '/'
-  base: '/NAMA-REPOSITORY/', 
+  base: '/My-Portfolio/', 
 })
